0eddccebb122e4fbf2d8e980fddce71b426cb67f
Arnaud Metral <arnaud.metral2@gmail.com>
2023-01-14 12:41:15 +0100

    bike model

---
+++ db/migrate/20230114114034_create_bikes.rb
@@ -0,0 +1,15 @@
+class CreateBikes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bikes do |t|
+      t.string :brand
+      t.string :model
+      t.float :price
+      t.string :location
+      t.boolean :electric
+      t.string :picture
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
