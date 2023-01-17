ace94d31834f812afe6d68ea82ba0a7ccd93975b
yamato belhouchet <yamato.belhouchet@yahoo.com>
2023-01-17 19:46:46 +0100

    validation added

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,4 +1,9 @@
 class Bike < ApplicationRecord
   belongs_to :user
   has_one_attached :picture
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :price, presence: true, numericality: true
+  validates :location, presence: true
+  validates :electric, inclusion: [true, false]
 end
--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,2 +1,8 @@
 class Rent < ApplicationRecord
+  belongs_to :bike
+  belongs_to :user
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :rent_price, presence: true, numericality: true
+  validates :status, presence: true
 end
