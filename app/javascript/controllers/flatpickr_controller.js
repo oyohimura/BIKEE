ac72aebe341cb29e1c5265ca77d7b3001e677ff5
Arnaud Metral <arnaud.metral2@gmail.com>
2023-01-24 21:22:42 +0100

    datepicker

---
+++ app/javascript/controllers/flatpickr_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+
+
+// Connects to data-controller="flatpickr"
+export default class extends Controller {
+  static targets = [ "startDate", "endDate" ]
+
+  connect() {
+    flatpickr(this.startDateTarget, {enableTime: true, time_24hr: true})
+    flatpickr(this.endDateTarget, {enableTime: true, time_24hr: true})
+  }
+}
