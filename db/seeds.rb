737c9bc567225e360644ed565d07b7f30c5a02fd
Clara Moriconi <oyohimura@gmail.com>
2023-01-26 19:52:09 +0100

    expanded seed with 4 user accounts and 6 bikes

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,9 @@ User.destroy_all
 
 puts "Destroying database!"
 
-puts "Generating example users..."
+# Creating users
+
+puts "Generating 4 example users..."
 user = User.new(
   password: 'Bob123$',
   email: 'bob@gmail.com',
@@ -32,43 +34,94 @@ user = User.new(
   last_name: 'Moriconi',
   address: 'rue de la montagne, Pyrenees'
 )
+
 file = URI.open("https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1663345881/yaxmecvbe7skaqrm4ziw.jpg")
 user.avatar.attach(io: file, filename: "clara.jpg", content_type: "image/jpg")
 user.save!
 
+user = User.new(
+  password: 'Yamato123$',
+  email: 'yamato@gmail.com',
+  first_name: 'Yamato',
+  last_name: 'Belhouchet',
+  address: 'rue du Japon, Paris'
+)
+
+file = URI.open("https://avatars.githubusercontent.com/u/113622459?v=4")
+user.avatar.attach(io: file, filename: "yamato.jpg", content_type: "image/jpg")
+user.save!
+
+user = User.new(
+  password: 'Arnaud123$',
+  email: 'arnaud@gmail.com',
+  first_name: 'Arnaud',
+  last_name: 'Metral',
+  address: 'rue du Brésil, Paris'
+)
+
+file = URI.open("https://avatars.githubusercontent.com/u/111865610?v=4")
+user.avatar.attach(io: file, filename: "arnaud.jpg", content_type: "image/jpg")
+user.save!
+
+# Creating bikes
+
 file = URI.open("https://cdn.shopify.com/s/files/1/0308/7024/1420/products/blue-R-1_870x580.jpg?v=1668396768")
 
-bike1 = Bike.new(brand: "Decathlon", model: "Riverside", location: "Paris", user_id: 1, price: 8, electric: true)
+bike1 = Bike.new(brand: "Decathlon", model: "Riverside", location: "Paris", price: 8, electric: true, user_id: 1)
 bike1.picture.attach(io: file, filename: "bike_decathlon.jpg", content_type: "image/jpg")
-bike1.user = User.first
+bike1.user = User.where(first_name: 'Bob').first
 bike1.save!
 
 puts "Generating first bike..."
 
 file = URI.open("https://performancevelo.com/23264-superlarge_default/peugeot-lc01-d7.jpg")
-bike2 = Bike.new(brand: "Peugeot", model: "T02", location: "Bordeaux", user_id: 1, price: 10, electric: false)
+bike2 = Bike.new(brand: "Peugeot", model: "T02", location: "Bordeaux", price: 10, electric: false, user_id: 1)
 bike2.picture.attach(io: file, filename: "bike_peugeot.jpg", content_type: "image/jpg")
-bike2.user = User.first
+bike2.user = User.where(first_name: 'Arnaud').first
 bike2.save!
 
 puts "Generating second bike..."
 
 file = URI.open("https://static.cyclelab.eu/velos/trek/2007/highres/FX3Disc_22_35021_A_Primary.jpg")
-bike3 = Bike.new(brand: "Trek", model: "FX 3", location: "Nantes", user_id: 1, price: 5, electric: false)
+bike3 = Bike.new(brand: "Trek", model: "FX 3", location: "Nantes", price: 5, electric: false, user_id: 1)
 bike3.picture.attach(io: file, filename: "bike_decathlon.jpg", content_type: "image/jpg")
-bike3.user = User.last
+bike3.user = User.where(first_name: 'Clara').first
 bike3.save!
 
 puts "Generating third bike..."
 
-puts "Generating example rent..."
+file = URI.open("https://m.media-amazon.com/images/I/71AgcYV8JBL._AC_SX679_.jpg")
+bike4 = Bike.new(brand: "EDEN-BIKES", model: "Pat Patrouille 2", location: "Grasse", price: 16, electric: false, user_id: 1)
+bike4.picture.attach(io: file, filename: "bike_pat_patrouille.jpg", content_type: "image/jpg")
+bike4.user = User.where(first_name: 'Yamato').first
+bike4.save!
+
+puts "Generating fourth bike..."
+
+file = URI.open("https://le-velo-urbain.com/wp-content/uploads/2018/03/selection-photo-velo-insolite-original-5.jpg")
+bike5 = Bike.new(brand: "Tordu", model: "Original model", location: "London", price: 36, electric: false, user_id: 1)
+bike5.picture.attach(io: file, filename: "bike_original.jpg", content_type: "image/jpg")
+bike5.user = User.where(first_name: 'Bob').first
+bike5.save!
+
+puts "Generating fifth bike..."
+
+file = URI.open("https://media.motoservices.com/media/cache/slider_lg/media/gallery/4520/Ryno-LeftRearAngle2-Rack.png")
+bike5 = Bike.new(brand: "Rhino", model: "One-Wheel", location: "Strasbourg", price: 24, electric: true, user_id: 1)
+bike5.picture.attach(io: file, filename: "bike_monocycle.jpg", content_type: "image/jpg")
+bike5.user = User.where(first_name: 'Yamato').first
+bike5.save!
+
+puts "Generating sixth bike..."
+
+puts "Generating 2 example rents..."
 rent = Rent.new(
   start_date: "2013-02-02 01:00:00 UTC",
   end_date: "2013-02-04 01:00:00 UTC",
   rent_price: 50.2
 )
-rent.user = User.first
-rent.bike = Bike.last
+rent.user = User.where(first_name: 'Yamato').first
+rent.bike = Bike.where(brand: 'Trek').first
 rent.save!
 
 rent = Rent.new(
@@ -76,8 +129,8 @@ rent = Rent.new(
   end_date: "2014-02-04 01:00:00 UTC",
   rent_price: 50.2
 )
-rent.user = User.last
-rent.bike = Bike.first
+rent.user = User.where(first_name: 'Clara').first
+rent.bike = Bike.where(brand: 'EDEN-BIKES').first
 rent.save!
 
 puts "Finished!"
