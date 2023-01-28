e156ba7ae8272200be78c83953df3572aa371f43
Clara Moriconi <oyohimura@gmail.com>
2023-01-28 14:26:21 +0100

    changed applicationrb

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module BIKEE
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     # config.middleware.use Rack::Synctime
-
+    config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
